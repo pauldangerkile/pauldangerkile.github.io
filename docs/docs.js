0daaa6ee25752d163afa133a41f979fab24318ec
Paul Danger Kile <dangerismymiddlename.com@gmail.com>
2024-06-16 15:52:39 -0500

    wip

--- docs/docs.js
+++ docs/docs.js
@@ -1,55 +1,59 @@
-var themes = [
-    'darkly',
-    'slate',
-    'superhero'
-];
+var themes = ["darkly", "slate", "superhero"];
 
 $(document).ready(function () {
-    $('[data-class]').click(function () {
-        updateNavbarClass($(this).attr('data-class'));
-    });
+	$("[data-class]").click(function () {
+		updateNavbarClass($(this).attr("data-class"));
+	});
 
-    updateNavbarClass('fixed-left');
+	updateNavbarClass("fixed-left");
 
-    themes.forEach(function (theme) {
-        $('#theme_select').append($('<option>', {
-            value: theme,
-            text: theme.charAt(0).toUpperCase() + theme.slice(1),
-            selected: theme === 'materia'
-        }));
-    });
+	themes.forEach(function (theme) {
+		$("#theme_select").append(
+			$("<option>", {
+				value: theme,
+				text: theme.charAt(0).toUpperCase() + theme.slice(1),
+				selected: theme === "darkly"
+			})
+		);
+	});
 });
 
 function updateNavbarClass(className) {
-    $('nav')
-        .removeClass(function (index, css) {
-            return (css.match(/(^|\s)fixed-\S+/g) || []).join(' ');
-        })
-        .addClass(className);
+	$("nav")
+		.removeClass(function (index, css) {
+			return (css.match(/(^|\s)fixed-\S+/g) || []).join(" ");
+		})
+		.addClass(className);
 
-    $('[data-class]').removeClass('active').parent('li').removeClass('active');
-    $('[data-class="' + className + '"]').addClass('active').parent('li').addClass('active');
+	$("[data-class]").removeClass("active").parent("li").removeClass("active");
+	$('[data-class="' + className + '"]')
+		.addClass("active")
+		.parent("li")
+		.addClass("active");
 
-    fixBodyMargin(className);
+	fixBodyMargin(className);
 }
 
 function fixBodyMargin(className) {
-    if (/fixed-(left|right)/.test(className)) {
-        $('body').removeAttr('style');
-        if (className === 'fixed-right') {
-            $('body').css('marginLeft', 0);
-        } else {
-            $('body').css('marginRight', 0);
-        }
-    } else {
-        $('body').css({
-            "margin-right": 0,
-            "margin-left": 0,
-            "padding-top": '90px'
-        });
-    }
+	if (/fixed-(left|right)/.test(className)) {
+		$("body").removeAttr("style");
+		if (className === "fixed-right") {
+			$("body").css("marginLeft", 0);
+		} else {
+			$("body").css("marginRight", 0);
+		}
+	} else {
+		$("body").css({
+			"margin-right": 0,
+			"margin-left": 0,
+			"padding-top": "90px"
+		});
+	}
 }
 
 function selectTheme(theme) {
-    $('#theme_link').attr('href', 'https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.3.1/' + theme + '/bootstrap.min.css');
+	$("#theme_link").attr(
+		"href",
+		"https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.3.1/" + theme + "/bootstrap.min.css"
+	);
 }
